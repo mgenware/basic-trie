f44ccc812a36de0d938cd7034741ed31e4d40710
Mgen <m@mgenware.com>
2020-11-06 23:41:56 +0800

    Add wildcard support

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,164 @@
-class Node<K, V> {
+/* eslint-disable class-methods-use-this */
+export class Node<K, V> {
   // The underlying map of this node.
   map = new Map<K, Node<K, V>>();
 
   // The value associated with this entry.
   value: V | null = null;
+
+  // A map containing all sub-routes for wildcards.
+  wildcardMap?: Map<K, Node<K, V>>;
 }
+
+export type PayloadType = Record<string, unknown>;
 
 export default class Trie<K, V> {
   #rootNode = new Node<K, V>();
 
+  // Whether logging is enabled.
+  logging = false;
+
   set(keys: K[], val: V): void {
+    this.log(`Calling set with keys "${JSON.stringify(keys)}", val: "${JSON.stringify(val)}"`);
+    this.checkKeys(keys);
     let node = this.#rootNode;
     for (const key of keys) {
-      const target = node.map.get(key);
-      if (target) {
-        node = target;
-      } else {
+      this.log(`Setting key "${key}"`);
+      if (this.isKeyWildcard(key)) {
+        if (!node.wildcardMap) {
+          node.wildcardMap = new Map<K, Node<K, V>>();
+        }
+        this.log(`Setting wildcard at key "${key}"`);
         const newNode = new Node<K, V>();
-        node.map.set(key, newNode);
+        node.wildcardMap.set(key, newNode);
         node = newNode;
+      } else {
+        const target = node.map.get(key);
+        if (target) {
+          this.log('Target found');
+          node = target;
+        } else {
+          this.log('Creating new node');
+          const newNode = new Node<K, V>();
+          node.map.set(key, newNode);
+          node = newNode;
+        }
       }
     }
     node.value = val;
   }
 
+  getWithPayload(keys: K[]): [V | undefined | null, PayloadType | undefined] {
+    this.log(`Calling getWithPayload with keys "${JSON.stringify(keys)}"`);
+    this.checkKeys(keys);
+    return this.getWithPayloadCore(this.#rootNode, keys, 0, undefined);
+  }
+
   get(keys: K[]): V | undefined | null {
-    let node = this.#rootNode;
-    for (const key of keys) {
-      const target = node.map.get(key);
-      if (target) {
-        node = target;
-      } else {
-        return undefined;
-      }
+    const [result] = this.getWithPayload(keys);
+    return result;
+  }
+
+  getWithPayloadCore(
+    node: Node<K, V>,
+    keys: K[],
+    index: number,
+    inheritedPayload: Readonly<PayloadType> | undefined,
+  ): [V | undefined | null, PayloadType | undefined] {
+    // Exit condition.
+    if (index >= keys.length) {
+      this.log(`Found the value "${node.value}", payload ${JSON.stringify(inheritedPayload)}`);
+      return [node.value, inheritedPayload];
     }
-    return node.value;
+
+    const key = keys[index];
+    this.logWithKey(key, `Getting value with key "${key}"`);
+
+    let firstAttemptResult: [V | undefined | null, PayloadType | undefined] = [
+      undefined,
+      undefined,
+    ];
+    const target = node.map.get(key);
+    if (target) {
+      this.logWithKey(key, 'Target found');
+      firstAttemptResult = this.getWithPayloadCore(target, keys, index + 1, inheritedPayload);
+    }
+
+    this.logWithKey(
+      key,
+      `Checking first attempt result "${JSON.stringify(firstAttemptResult[0])}"`,
+    );
+    if (firstAttemptResult[0] !== undefined && firstAttemptResult[0] !== null) {
+      this.logWithKey(key, `Found the value at first attempt "${firstAttemptResult[0]}"`);
+      return firstAttemptResult;
+    }
+
+    // Nothing at first attempt.
+    // Try wildcard match.
+    const { wildcardMap } = node;
+    if (wildcardMap) {
+      let secondAttemptResult: [V | undefined | null, PayloadType | undefined] = [
+        undefined,
+        undefined,
+      ];
+      for (const [nodeKey, nodeValue] of wildcardMap.entries()) {
+        this.logWithKey(key, `Keep searching with child entry "${nodeKey}" in wildcard mode`);
+
+        const payload = {
+          ...inheritedPayload,
+          ...this.getWildcardPayload(key, nodeKey, nodeValue.value),
+        };
+        this.logWithKey(
+          key,
+          `Handling wildcard, key "${key}", value "${node.value}" payload ${JSON.stringify(
+            payload,
+          )}`,
+        );
+        secondAttemptResult = this.getWithPayloadCore(nodeValue, keys, index + 1, payload);
+        this.logWithKey(
+          key,
+          `Checking second attempt result "${JSON.stringify(secondAttemptResult[0])}"`,
+        );
+        if (secondAttemptResult[0] !== undefined && secondAttemptResult[0] !== null) {
+          return secondAttemptResult;
+        }
+      }
+
+      this.logWithKey(key, 'Nothing found in wildcard mode');
+      // Nothing found, return second attempt result as it might be null
+      // instead of undefined when keys are parts of a path.
+      return secondAttemptResult;
+    }
+    this.logWithKey(key, 'Nothing found');
+    return firstAttemptResult;
+  }
+
+  protected isKeyWildcard(_key: K): boolean {
+    return false;
+  }
+
+  protected getWildcardPayload(
+    _key: K,
+    _wildcardName: K,
+    _wildcardValue: V | null,
+  ): PayloadType | undefined {
+    return undefined;
+  }
+
+  private checkKeys(keys: K[]) {
+    if (!keys?.length) {
+      throw new Error(`Empty keys, got "${JSON.stringify(keys)}"`);
+    }
+  }
+
+  private log(s: string) {
+    if (this.logging) {
+      // eslint-disable-next-line no-console
+      console.log(s);
+    }
+  }
+
+  private logWithKey(key: K, msg: string) {
+    this.log(`KEY: ${key} | ${msg}`);
   }
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,6 @@
+/* eslint-disable class-methods-use-this */
 import * as assert from 'assert';
-import Trie from '..';
+import Trie, { PayloadType } from '..';
 
 it('Main', () => {
   const trie = new Trie<number, string>();
@@ -11,4 +12,56 @@ it('Main', () => {
   assert.strictEqual(trie.get([1, 2, 3, 4]), undefined);
   assert.strictEqual(trie.get([1]), null);
   assert.strictEqual(trie.get([2, 1]), undefined);
+});
+
+class MyTrie extends Trie<string, string> {
+  isKeyWildcard(key: string): boolean {
+    return key.startsWith(':');
+  }
+
+  getWildcardPayload(
+    key: string,
+    wildcardName: string,
+    _wildcardValue: string | null,
+  ): PayloadType | undefined {
+    return {
+      [wildcardName.substr(1)]: key,
+    };
+  }
+}
+
+function splitPathString(s: string): string[] {
+  return s.split('/');
+}
+
+function addMyPath(trie: MyTrie, s: string) {
+  trie.set(splitPathString(s), s);
+}
+
+it('Wildcard: single wildcard', () => {
+  const trie = new MyTrie();
+  trie.logging = true;
+  addMyPath(trie, ':s');
+  assert.deepStrictEqual(trie.getWithPayload(['a']), [':s', { s: 'a' }]);
+  assert.deepStrictEqual(trie.getWithPayload([':s']), [':s', { s: ':s' }]);
+  assert.deepStrictEqual(trie.getWithPayload(['a', 'b']), [undefined, undefined]);
+});
+
+it('Wildcard: multiple wildcards', () => {
+  const trie = new MyTrie();
+  trie.logging = true;
+  addMyPath(trie, ':a/:b');
+  assert.deepStrictEqual(trie.getWithPayload(['1', '2']), [':a/:b', { a: '1', b: '2' }]);
+  assert.deepStrictEqual(trie.getWithPayload(['1', '2', '3']), [undefined, undefined]);
+  assert.deepStrictEqual(trie.getWithPayload(['1']), [null, { a: '1' }]);
+});
+
+it('Wildcard: same level', () => {
+  const trie = new MyTrie();
+  trie.logging = true;
+  addMyPath(trie, ':a');
+  addMyPath(trie, ':b');
+  assert.deepStrictEqual(trie.getWithPayload(['a']), [':s', { s: 'a' }]);
+  assert.deepStrictEqual(trie.getWithPayload([':s']), [':s', { s: ':s' }]);
+  assert.deepStrictEqual(trie.getWithPayload(['a', 'b']), [undefined, undefined]);
 });
